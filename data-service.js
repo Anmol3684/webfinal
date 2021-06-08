fadb8cf084b581959347bfb9a5c3b1a6b48eba9f
nyildirim <71366803+nyildirim@users.noreply.github.com>
2021-06-08 19:16:52 -0400

    last commit

---
+++ data-service.js
@@ -0,0 +1,57 @@
+const fs = require ("fs");
+const { resolve } = require("path");
+let  employees = [];
+let departments = [];
+
+module.exports.initialize = function(){
+  return new Promise((resolve,reject)=>{
+    fs.readFile('./data/employees.json',(err,data) =>{
+      if(err){
+        reject(err)
+      }
+      employees = JSON.parse(data);
+    })
+    fs.readFile('./data/departments.json',(err, data)=>{
+      if(err){
+        reject(err);
+      }
+      departments = JSON.parse(data);
+    })
+      resolve();
+    });
+}
+//EMPLOYEES
+module.exports.getAllEmployees = function(){
+
+  return new Promise((resolve,reject)=>{
+   if (employees.length == 0) {
+   reject("No data to be displayed");
+  }
+            resolve(employees);
+});
+}
+//MANAGERS
+module.exports.getManagers = function(){
+  return new Promise((resolve,reject) =>{
+    var managers = [];
+    for(let i= 0; i<employees.length; i++){
+      if(employees[i].isManager==true){
+        managers.push(employees[i]);
+        }
+    }
+      if(managers.length == 0){
+        reject("No managers to be displayed");
+      }
+        resolve(managers);
+    });
+  }
+//DEPARTMENTS
+module.exports.getDepartments = function(){
+
+  return new Promise((resolve,reject)=>{
+   if (departments.length == 0) {
+   reject("No data to be displayed");
+  }
+            resolve(departments);
+});
+}
---
+++ server.js
@@ -0,0 +1,57 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part 
+* of this assignment has been copied manually or electronically from any other source 
+* (including 3rd party web sites) or distributed to other students.
+* 
+* Name: _______Nurten YILDIRIM_______________ Student ID: ____141346197__________ Date: ___2021/06/10_____________
+*
+* Online (Heroku) Link: ________________________________________________________
+*
+********************************************************************************/
+var express = require("express");
+var app = express();
+var path = require("path")
+const data = require('./data-service.js');
+app.use(express.static('public'));
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+app.use(express.static('public'));
+
+
+// setup  route to listen on /about
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/about.html"));
+});
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+});
+app.get("/employees",(req,res) =>{
+    data.getAllEmployees().then((data)=>{
+        res.json(data);
+    });
+});
+app.get("/departments",(req,res) =>{
+    data.getDepartments().then((data)=>{
+        res.json(data);
+    });
+});
+app.get("/managers",(req,res) =>{
+    data.getManagers().then((data)=>{
+        res.json(data);
+    });
+});
+app.use((req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/error.html"));
+});
+ 
+    data.initialize().then(function(){
+    app.listen(HTTP_PORT, onHttpStart);
+}).catch(function(err){
+    console.log("Unable to start server:" + err);
+});
